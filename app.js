dd1280c67bea7ed335b8af95f1fde7cdadcf8e83
mdemon420 <mdeamon4@gmail.com>
2021-05-27 23:03:05 +0600

    lyric button

--- app.js
+++ app.js
@@ -9,17 +9,24 @@ const searchSongs = () => {
 const displaySong = songs => {
     const songContainer = document.getElementById("song-container");
    songs.forEach(song =>{
+       console.log(song)
        const div = document.createElement("div");
        div.className ="single-result row align-items-center my-3 p-3";
        div.innerHTML = `
             <div class="col-md-9">
                  <h3 class="lyrics-name">${song.title}</h3>
                 <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                <audio controls>
+                    <source src="${song.preview}" type="audio/mpeg">
+                </audio>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                 <button class="btn btn-success">Get Lyrics</button>
+                 <button onclick="getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
             </div>
        `;
        songContainer.appendChild(div);
    });
-};
+}
+const getLyrics =(artist, title)=>{
+    console.log(artist,title);
+}   
